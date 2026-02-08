71b86073e4c0c2700ecc269794c29c569d99591c
david.nieto <davidnietoo@gmail.com>
2026-02-09 00:38:49 +0100

    refactor: inject `HttpClient` dependency and use its instance for API calls.

--- src/modules/cart/infrastructure/cart-repository-api.ts
+++ src/modules/cart/infrastructure/cart-repository-api.ts
@@ -1,10 +1,15 @@
 import { HttpClient } from '@/modules/shared/infrastructure/http-client';
 import { CartRepository } from '../domain/cart-repository';
 import { AddToCartResponseSchema } from './cart-dto';
+import { inject, IID } from '@/modules/shared/infrastructure/bootstrap/IID';
 
 export class CartRepositoryApi implements CartRepository {
+    constructor(
+        private readonly httpClient: HttpClient = inject(IID.httpClient)
+    ) { }
+
     async addToCart(productId: string, colorCode: number, storageCode: number): Promise<number> {
-        const response = await HttpClient.post<{ count: number }>('/cart', {
+        const response = await this.httpClient.post<{ count: number }>('/cart', {
             id: productId,
             colorCode,
             storageCode

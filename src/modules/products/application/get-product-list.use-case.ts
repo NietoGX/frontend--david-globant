635098c3d534c1ed7edb98939a74b1fee214204b
david.nieto <davidnietoo@gmail.com>
2026-02-08 15:09:33 +0100

    feat: improve filtering by brand and model

--- src/modules/products/application/get-product-list.use-case.ts
+++ src/modules/products/application/get-product-list.use-case.ts
@@ -3,7 +3,7 @@ import { Product } from '../domain/product';
 import { inject, IID } from '@/modules/shared/infrastructure/bootstrap/IID';
 
 export class GetProductList {
-    constructor(private readonly repository = inject(IID.productRepository)) {}
+    constructor(private readonly repository = inject(IID.productRepository)) { }
 
     async execute(searchTerm?: string): Promise<Product[]> {
         const products = await this.repository.getProducts();
@@ -12,12 +12,11 @@ export class GetProductList {
             return products;
         }
 
-        const term = searchTerm.toLowerCase();
-        return products.filter(product => {
-            const matchBrand = product.brand.toLowerCase().includes(term);
-            const matchModel = product.model.toLowerCase().includes(term);
+        const terms = searchTerm.toLowerCase().split(' ').filter(t => t.length > 0);
 
-            return matchBrand || matchModel;
+        return products.filter(product => {
+            const productText = `${product.brand} ${product.model}`.toLowerCase();
+            return terms.every(term => productText.includes(term));
         });
     }
 }
